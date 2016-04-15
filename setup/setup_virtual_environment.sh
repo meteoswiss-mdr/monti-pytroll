21110b47fbd464cbafa09dc713320307ba73bd2f
hau <ulrich.hamann@meteoswiss.ch>
2016-04-15 12:49:05 +0000

    use no version nr for python packages

--- setup/setup_virtual_environment.sh
+++ setup/setup_virtual_environment.sh
@@ -15,24 +15,22 @@
 # version 0.1: 16-02-2016 U. Hamann
 
 # set paths for anaconda (again, for security)
-export PYTROLLHOME=/opt/users/$LOGNAME/PyTroll/
-export PATH=$PYTROLLHOME/packages/anaconda2/bin:$PATH
-export PYTHONPATH=$PYTROLLHOME/packages/anaconda2/bin:$PYTROLLHOME/scripts
+. ./setup_anaconda_zueub428.sh
 
-echo "*** Switch off SSL check for conda installations"
-conda config --set ssl_verify false
+#echo "*** Switch off SSL check for conda installations"
+#conda config --set ssl_verify false
 
-echo "*** Update anaconda itself"
-conda update conda
+#echo "*** Update anaconda itself"
+#conda update conda
 
 echo "*** Create virtual environement and install python packages according to PyTroll-conda-package-list.txt"
-conda create -n PyTroll --file PyTroll-conda-package-list.txt
+conda create -n PyTroll_$(logname) --file PyTroll-conda-package-list_no_version_nr.txt
 
 echo "*** Activate virtual environment"
-source activate PyTroll
+source activate PyTroll_$(logname)
 
 echo "*** Pip installation of additional package"
-pip install --trusted-host pypi.python.org -r PyTroll-pip-requirements.txt
+pip install --trusted-host pypi.python.org -r PyTroll-pip-requirements_no_version_nr2.txt
 
 echo "*** Deactivate virtual environment"
 source deactivate

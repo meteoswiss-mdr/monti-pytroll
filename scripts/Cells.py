a67297a6d9b40b0a1167393dfbb9a9c92b537689
hau <ulrich.hamann@meteoswiss.ch>
2016-07-14 15:45:34 +0000

    add cell class as a file

---
+++ scripts/Cells.py
@@ -0,0 +1,14 @@
+class Cells:
+        def __init__(self):
+            self.idCell         = []
+            self.mean108        = []
+            self.t_start        = []
+            self.id_prev        = []
+            self.split          = []
+            self.t_end          = []
+            self.origin         = []
+            self.end            = []
+            self.area_px        = []
+            self.center         = []   
+            self.merge          = []
+            self.colors         = []

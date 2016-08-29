2f9a279651230ef3c733fb8e1b9f54e2d0b894fb
hau <ulrich.hamann@meteoswiss.ch>
2016-08-29 09:56:59 +0000

    no redirecting inside the plot_c2 script any more

--- scripts/plot_c2_cronjob.sh
+++ scripts/plot_c2_cronjob.sh
@@ -2,34 +2,23 @@
 ### ### #!/bin/bash
 
 # go to operational pytroll folder  
-#export PYTHONPATH=/opt/users/common/packages/anaconda2/bin:/opt/users/hau/PyTroll//scripts
-#export XRIT_DECOMPRESS_PATH=/opt/users/common/bin/xRITDecompress
-#export PPP_CONFIG_DIR=/opt/users/hau/PyTroll//etc/
 echo '***'
 echo '*** start plot_c2_cronjob.sh'
 . /opt/users/hau/PyTroll/setup/bashrc
 #export python=/usr/bin/python
-export python=/opt/users/common/packages/anaconda3/envs/PyTroll_$LOGNAME/bin/python
 
-export dir1=/opt/users/hau/PyTroll/scripts
+export python=/opt/users/common/packages/anaconda3/envs/PyTroll_$LOGNAME/bin/python
+export dir1=/opt/users/$LOGNAME/PyTroll/scripts
 cd ${dir1}
 
 echo "*** Start to make seviri pictures (loop until all data is there)"
 echo 
-#$python ${dir1}/plot_msg.py input_c2_background # > /tmp/loop_msg.txt 2>&1
-$python ${dir1}/plot_coalition2_ccs4.py > /tmp/plot_coalition2_ccs4.txt 2>&1
-$python ${dir1}/plot_coalition2_EuropeS95.py > /tmp/plot_coalition2_EuropeS95.txt 2>&1
-#$python ${dir1}/loop_msg.py input_c2_background # > /tmp/loop_msg.txt 2>&1
-#$python ${dir1}/loop_msg.py input_nwc_pngfiles 
-
-## execute plot_msg pytroll script once (to create nc files)
-#export PPP_CONFIG_DIR=/data/OWARNA/hau/pytroll/cfg_offline
-#$python ${dir1}/plot_msg.py input_rad_ncfiles # > /tmp/plot_msg.txt 2>&1
-#$python ${dir1}/plot_msg.py input_nwc_ncfiles
+$python ${dir1}/plot_coalition2.py input_coalition2 #> /tmp/plot_coalition2.txt 2>&1
 
 # remove uncompressed data older than 10min
-echo "*** Remove uncompressed data files older than 12min"
-#find /tmp/?-000-MSG?__-MSG?_???____-*_-*___-*-__ -type f -mmin +15 -exec rm {} \;
-find /tmp/?-000-MSG?__-MSG?_???____-*_-*___-*-__  -type f -mmin +40 -delete \;
+export mm=50
+echo "*** Remove uncompressed data files older than " ${mm} "min"
+#find /tmp/?-000-MSG?__-MSG?_???____-*_-*___-*-__ -type f -mmin $mm -exec rm {} \;
+find /tmp/?-000-MSG?__-MSG?_???____-*_-*___-*-__  -type f -mmin ${mm} -delete \;
 
-find /tmp/SEVIRI_DECOMPRESSED_hau/?-000-MSG?__-MSG?_???____-*_-*___-*-__ -type f -mmin +40 -delete
+find /tmp/SEVIRI_DECOMPRESSED_hau/?-000-MSG?__-MSG?_???____-*_-*___-*-__ -type f -mmin ${mm} -delete

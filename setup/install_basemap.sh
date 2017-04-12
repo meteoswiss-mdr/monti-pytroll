9bf3789f6f3aa50bee4d35d52d2eeeba2dc9e15b
hau <ulrich.hamann@meteoswiss.ch>
2017-04-12 08:20:43 +0000

    slower git cloning for basemap as error occured

--- setup/install_basemap.sh
+++ setup/install_basemap.sh
@@ -15,13 +15,26 @@
 export INSTALL_DIR=/opt/users/common/packages
 cd $INSTALL_DIR
 
+## problems when cloning basemap
+#
 #git clone https://github.com/matplotlib/basemap.git
+#
+##fatal: The remote end hung up unexpectedly
+##fatal: early EOF
+##fatal: index-pack failed
+## hence do some git settings before cloning
+git config --global core.compression 0
+git clone --depth 1 https://github.com/matplotlib/basemap.git
+cd $INSTALL_DIR/basemap/basemap
+git fetch --unshallow
+git pull --all
 
 ## install geos library
 mkdir -p $INSTALL_DIR/basemap/GEOS-3.3.3
 export GEOS_DIR=$INSTALL_DIR/basemap/GEOS-3.3.3
 cd $INSTALL_DIR/basemap/geos-3.3.3
 # python bindung would require installation of SWIG  http://www.swig.org/exec.html
+# with --enable-python error while make: -lpython library not found 
 ./configure --prefix=$GEOS_DIR  # --enable-python --enable-swig
 make; make install
 

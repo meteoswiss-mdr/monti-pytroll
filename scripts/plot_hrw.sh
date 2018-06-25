6b07cbcf63efe2729fd499230de6fcae0f879a4d
cinesat <cinesat@zueub427.meteoswiss.ch>
2018-06-25 10:55:46 +0000

    date input possible for shell script

--- scripts/plot_hrw.sh
+++ scripts/plot_hrw.sh
@@ -4,7 +4,7 @@
 #----------------
 
 echo '***'
-echo '*** start plot_lightning.sh'
+echo '*** start plot_hrw.sh'
 . /opt/users/$LOGNAME/monti-pytroll/setup/bashrc no_virtual_environment
 #export python=/usr/bin/python
 #export python=/opt/users/common/packages/anaconda3/envs/PyTroll_$LOGNAME/bin/python
@@ -12,7 +12,9 @@ echo '*** start plot_lightning.sh'
 cd $PYTROLLHOME/scripts
 
 echo "*** Start to make high resolution wind images"
-python plot_hrw_cronjob.py # > /tmp/plot_msg.txt 2>&1
+python plot_hrw_cronjob.py input_hrw_cronjob.py $1 $2 $3 $4 $5  # > /tmp/plot_msg.txt 2>&1
+
+#python postprocessing.py --parea ccs4  input_hrw_cronjob.py
 
 ## remove result files older than 1 hour (done in plot_ms.sh)
 #echo "*** Remove all result files older than 1 hour"

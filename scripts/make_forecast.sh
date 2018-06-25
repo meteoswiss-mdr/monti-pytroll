66059cce5aab3761dc2bb2278bd0fe09a920d501
cinesat <cinesat@zueub427.meteoswiss.ch>
2018-06-25 12:44:45 +0000

    use no_virtual_envoronment option for case studies

--- scripts/make_forecast.sh
+++ scripts/make_forecast.sh
@@ -2,20 +2,21 @@
 
 dir_=${PWD}
 
-year=2015
+year=2018
 y2=$(echo "$year-2000" | bc)
-month=6
+month=5
 MM="$(printf "%02d" $month)"
 
 export python=/opt/users/common/packages/anaconda3/envs/PyTroll_$LOGNAME/bin/python
 #export python=/usr/bin/python
 
-. /opt/users/$LOGNAME/PyTroll/setup/bashrc_offline
+#. /opt/users/$LOGNAME/PyTroll/setup/bashrc_offline
+. /opt/users/$LOGNAME/PyTroll/setup/bashrc nve
 
-for (( d = 6; d <= 7; d++ ))
+for (( d = 9; d <= 9; d++ ))
 do 
     dd="$(printf "%02d" $d)"
-    for (( h = 0; h <= 23; h=h+1 ))
+    for (( h = 11; h <= 12; h=h+1 ))
     do 
 	hh="$(printf "%02d" $h)"
  	for (( m = 0; m <= 55; m=m+5 ))

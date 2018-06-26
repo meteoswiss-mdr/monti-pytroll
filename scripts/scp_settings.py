becb89001758b63112d38234b153e56864ddc089
cinesat <cinesat@zueub427.meteoswiss.ch>
2018-06-26 15:14:04 +0000

    use PYTROLLHOME variable for scp settings

--- scripts/scp_settings.py
+++ scripts/scp_settings.py
@@ -1,2 +1,6 @@
-scpOutputDir="las@lomux240:/www/proj/OTL/WOL/cll/satimages"
-scpID="-i /opt/users/$LOGNAME/monti-pytroll/scripts/id_dsa_las"
+scpOutputDir="las@zueub241:/srn/las/www/satellite/DATA/"
+scpID="-i $PYTROLLHOME/scripts/id_rsa_las"
+#scpOutputDir="las@lomux240:/www/proj/OTL/WOL/cll/satimages"
+#scpID="-i /opt/users/$LOGNAME/monti-pytroll/scripts/id_dsa_las"
+#scpOutputDir="las@zueub241:/srn/las/www/satellite/DATA/"
+#scpID="-i /opt/users/$LOGNAME/monti-pytroll/scripts/id_rsa_las"

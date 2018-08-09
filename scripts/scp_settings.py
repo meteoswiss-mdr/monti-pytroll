a297ce77bb5d1d4e3cce70739ab1d878aebbfe5c
cinesat <cinesat@zueub427.meteoswiss.ch>
2018-08-09 13:56:02 +0000

    keep / at the end of the folder name

--- scripts/scp_settings.py
+++ scripts/scp_settings.py
@@ -1,3 +1,5 @@
+# the slash / in the end produce a warning, if the dir does not exist
+# without / it is assumed that it is a filename... 
 scpOutputDir="las@zueub241:/srn/las/www/satellite/DATA/MSG_%(rgb)s-%(area)s_/"
 scpID="-i $PYTROLLHOME/scripts/id_rsa_las"
 #scpOutputDir="las@lomux240:/www/proj/OTL/WOL/cll/satimages"

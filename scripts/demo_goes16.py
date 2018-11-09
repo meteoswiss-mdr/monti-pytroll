bfe34d259fee4b8178e9926527aac9c9bb85f368
cinesat <cinesat@zueub428.meteoswiss.ch>
2018-11-09 15:32:49 +0000

    add different location for example file

--- scripts/demo_goes16.py
+++ scripts/demo_goes16.py
@@ -1,4 +1,13 @@
 from satpy import Scene
 from glob import glob
-filenames = glob("/store/msrad/sat/goes-16/2017/04/05/*s20170952100319_e20170952100377*")
+import socket
+
+if 'zueub' in socket.gethostname():
+    #MeteoSwiss
+    filenames = glob("/data/COALITION2/database/goes-16/2017/04/05/*s20170952100319_e20170952100377*")
+else:
+    #kesch/CSCS
+    filenames = glob("/store/msrad/sat/goes-16/2017/04/05/*s20170952100319_e20170952100377*")
+
+print filenames
 global_scene = Scene(reader="abi_l1b", filenames=filenames)

4c8e2c225b75eca92c8d33e4310155544db3e34a
hau <ulrich.hamann@meteoswiss.ch>
2018-11-09 15:34:50 +0000

    add new example file

--- scripts/demo_goes16.py
+++ scripts/demo_goes16.py
@@ -1,4 +1,23 @@
+
 from satpy import Scene
 from glob import glob
-filenames = glob("/store/msrad/sat/goes-16/2017/04/05/*s20170952100319_e20170952100377*")
+
+filenames = glob("/data/COALITION2/database/goes-16/2017/04/05/*s20170952100319_e20170952100377*")
+#filenames = glob("/store/msrad/sat/goes-16/2017/04/05/*s20170952100319_e20170952100377*")
+
 global_scene = Scene(reader="abi_l1b", filenames=filenames)
+print global_scene.available_composite_names()
+
+#['airmass', 'ash', 'dust', 'fog', 'green', 'green_crefl', 'green_raw', 'green_snow', 'ir108_3d', 'ir_cloud_day', 'natural', 'natural_color', 'natural_color_raw', 'natural_color_sun', 'natural_sun', 'night_microphysics', 'overview', 'overview_raw', 'true_color', 'true_color_crefl', 'true_color_raw']
+
+# the following does not work ???
+##################################
+
+#rgb='true_color' # needs /u/37482654/rayleigh_only/rayleigh_luts_rayleigh_only.tgz which fails to download automatically
+#rgb='overview' # does not exist
+rgb='airmass'
+### rgb='VIS006' ... unknown dataset ...
+global_scene.load([rgb])
+global_scene[rgb].show()
+local_scene = global_scene.resample("eurol")
+local_scene.show(rgb)

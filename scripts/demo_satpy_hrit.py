568e1d80c571e716c2194b0dd9aaedbd718f5e6f
hau <ulrich.hamann@meteoswiss.ch>
2018-12-06 16:18:01 +0000

    updated demo scripts

---
+++ scripts/demo_satpy_hrit.py
@@ -0,0 +1,75 @@
+
+#from satpy import Scene
+from satpy.utils import debug_on
+debug_on()
+
+#from glob import glob
+#base_dir="/data/COALITION2/database/meteosat/radiance_HRIT/case-studies/2015/07/07/"
+#import os
+#os.chdir(base_dir)
+#filenames = glob("*201507071200*__")
+#print base_dir
+#print filenames
+##global_scene = Scene(reader="hrit_msg", filenames=filenames, base_dir=base_dir, ppp_config_dir="/opt/users/hau/PyTroll//cfg_offline/")
+#global_scene = Scene(reader="hrit_msg", filenames=filenames, base_dir=base_dir, ppp_config_dir="/opt/users/hau/PyTroll/packages/satpy/satpy/etc")
+
+
+# new version of satpy after 0.8
+#################################
+from satpy import Scene, find_files_and_readers
+from datetime import datetime
+
+files_sat = find_files_and_readers(sensor='seviri',
+                               start_time=datetime(2015, 7, 7, 12, 0),
+                               end_time=datetime(2015, 7, 7, 12, 0),
+                               base_dir="/data/COALITION2/database/meteosat/radiance_HRIT/case-studies/2015/07/07/",
+                               reader='hrit_msg')
+
+#print files_sat
+#files = dict(files_sat.items() + files_nwc.items())
+files = dict(files_sat.items())
+
+global_scene = Scene(reader="hrit_msg", filenames=files)
+
+print dir(global_scene)
+
+#global_scene.load([0.6, 0.8, 10.8])
+#global_scene.load(['IR_120', 'IR_134'])
+global_scene.load(['overview',0.6,0.8])
+#print(global_scene[0.6])            # works only if you load also the 0.6 channel, but not an RGB that contains the 0.6
+#!!# print(global_scene['overview']) ### this one does only work in the develop version
+
+global_scene.available_dataset_names()
+
+global_scene["ndvi"] = (global_scene[0.8] - global_scene[0.6]) / (global_scene[0.8] + global_scene[0.6])
+# !!! BUG: will not be resampled in global_scene.resample(area)
+
+#from satpy import DatasetID
+#my_channel_id = DatasetID(name='IR_016', calibration='radiance')
+#global_scene.load([my_channel_id])
+#print(scn['IR_016'])
+
+#area="eurol"
+area="EuropeCanaryS95"
+local_scene = global_scene.resample(area)
+
+print global_scene.available_composite_names()
+
+#local_scene.show('overview')
+local_scene.save_dataset('overview', './overview_'+area+'.png')
+print 'display ./overview_'+area+'.png &'
+
+local_scene["ndvi"] = (local_scene[0.8] - local_scene[0.6]) / (local_scene[0.8] + local_scene[0.6])
+local_scene.save_dataset('ndvi', './ndvi_'+area+'.png')
+print dir(local_scene.save_dataset)
+#from satpy.enhancements import colorize
+#colorize(img, **kwargs)
+#'ylgn'
+#https://satpy.readthedocs.io/en/latest/writers.html
+#nice NDVI colourbar here:
+#https://www.researchgate.net/figure/NDVI-maps-Vegetation-maps-created-by-measuring-the-Normalized-Vegetation-Difference_fig7_323885082
+
+print 'display ./ndvi_'+area+'.png &'
+
+
+
---
+++ scripts/demo_satpy_nwcsaf.py
@@ -0,0 +1,70 @@
+
+#from satpy.utils import debug_on
+#debug_on()
+
+import nwcsaf
+#product = {}
+#product['CMA']     = [ 'cloudmask']
+
+product_list = ['CTTH']
+## 'ASII-NG' and 'RDT-CW' do not work!!
+# sam's product_list = ['CMA', 'CT', 'CTTH', 'CMIC', 'PC', 'CRR', 'iSHAI', 'CI', 'RDT-CW', 'ASII-NG']
+# product_list = ['ASII-NG', 'CI', 'CMA', 'CMIC', 'CRR', 'CRR-Ph', 'CT', 'CTTH', 'HRW', 'iSHAI', 'PC', 'PC-Ph', 'PLAX', 'RDT-CW']
+# product.keys() ['ASII-NG', 'CI', 'CMA', 'CMIC', 'CRR', 'CRR-Ph', 'CT', 'CTTH', 'HRW', 'iSHAI', 'PC', 'PC-Ph', 'RDT-CW']
+
+# Marco Sassi's program
+# zueub428:/opt/pytroll/nwcsaf/nwcsaf-processing/bin/NWCSAF_processing.py
+# old version: zueub428:/opt/safnwc/bin/NWCSAF_processing.py
+
+from satpy import Scene, find_files_and_readers
+from datetime import datetime
+
+for p_ in product_list:
+
+    start_time=datetime(2017, 7, 7, 12, 0)
+    end_time=datetime(2017, 7, 7, 12, 0)
+    
+    #file_pattern='S_NWC_' + product + '_' + sat_id + '_*_' + timestamp_NWCSAF + extension
+    #if glob.glob(file_pattern):
+    #    print ("NWCSAF data product " + product + " exists and is readable")
+    #    # creates list of available products
+    #    product_list_to_process.append(nwcsaf.product.get(product)[:])
+    #else:
+    #    print ("NWCSAF data product " + product + " is missing or is not readable")
+    
+    print "... search files in "+ "/data/COALITION2/database/meteosat/SAFNWC_v2016/2017/07/07/"+p_+"/"
+    files_nwc = find_files_and_readers(sensor='seviri',
+                                       start_time=start_time,
+                                       end_time=end_time,
+                                       #base_dir="/data/COALITION2/database/meteosat/SAFNWC_v2013/2015/07/07/",
+                                       base_dir="/data/COALITION2/database/meteosat/SAFNWC_v2016/2017/07/07/"+p_+"/",
+                                       reader='nc_nwcsaf_msg')
+    #print files_nwc
+    #files = dict(files_sat.items() + files_nwc.items())
+    files = dict(files_nwc.items())
+
+    global_scene = Scene(filenames=files)
+
+    #global_scene.load(['cloudtype'])
+    global_scene.load(nwcsaf.product[p_])
+
+    global_scene.available_dataset_names()
+    #!!# print(global_scene['overview']) ### this one does only work in the develop version
+    print ""
+    print "available_composite_names"
+    print global_scene.available_composite_names()
+
+    local_scene = global_scene.resample("ccs4")
+
+    for p_name in nwcsaf.product[p_]:
+        
+        #local_scene.show('cloudtype')
+        #local_scene.save_dataset('cloudtype', './local_cloudtype.png')
+        #print "display ./local_cloudtype.png &"
+
+        #local_scene.show(p_name)
+        #local_scene.show(p_name, overlay={'coast_dir': '/data/OWARNA/hau/maps_pytroll/', 'color': (255, 0, 0), 'resolution': 'i'})
+        local_scene.save_dataset(p_name, "./local_"+p_name+".png", overlay={'coast_dir': '/data/OWARNA/hau/maps_pytroll/', 'color': (255, 0, 0), 'resolution': 'i'})
+        print "display ./local_"+p_name+".png &"
+
+

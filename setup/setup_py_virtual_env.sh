5dec391f737236193f529527b3da12e45dd33e48
hau <ulrich.hamann@meteoswiss.ch>
2019-04-05 14:33:32 +0000

    basic commands to start python virutal env

---
+++ setup/setup_py_virtual_env.sh
@@ -0,0 +1,15 @@
+pip install --user --trusted-host nexus.meteoswiss.ch --install-option="--prefix=/opt/users/hau/python-packets" virtualenv
+
+export ENV_PATH=/opt/users/hau/C2python2_env
+#echo $ENV_PATH
+mkdir -p $ENV_PATH
+
+python -m virtualenv $ENV_PATH
+
+source $ENV_PATH/bin/activate
+
+# This does NOT work: 
+# pip install --trusted-host nexus.meteoswiss.ch --install-option="--prefix=/opt/users/hau/python-packets" virtualenvwrapper
+#   File "/tmp/pip-install-JXHTwV/virtualenv-clone/setup.py", line 9, in <module>
+#   with open('README.md') as f:
+#   IOError: [Errno 2] No such file or directory: 'README.md'

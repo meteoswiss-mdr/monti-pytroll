60b872e07e1d275c6aa552fed9bc94007181c233
cinesat <cinesat.cinesat@meteoswiss.ch>
2019-06-06 14:02:30 +0000

    replaced channel number with name
    still have to investigate, why micrometer numbers dont work anymore

--- scripts/my_composites.py
+++ scripts/my_composites.py
@@ -239,7 +239,7 @@ def DayNightFog(self, downscale=False, sza_max=88):
     during day:   HRVFog
     during night: night microphysics
     """
-    self.check_channels(1.6, 3.9, 10.8, 12.0, "HRV")
+    self.check_channels("IR_016", "IR_039", "IR_108", "IR_120", "HRV")
 
     # HRVFog recipe
     # --------------
@@ -249,7 +249,7 @@ def DayNightFog(self, downscale=False, sza_max=88):
     import numpy as np
     cos_sza = np.cos(np.radians(sza)) + 0.05
         
-    ch1a = self[1.6].data   / cos_sza
+    ch1a = self["IR_016"].data   / cos_sza
     ch2a = self["HRV"].data / cos_sza
     ch3a = self["HRV"].data / cos_sza
 
@@ -261,9 +261,9 @@ def DayNightFog(self, downscale=False, sza_max=88):
 
     # night microphysics recipe
     # --------------------------       
-    ch1b = self[12.0].data - self[10.8].data
-    ch2b = self[10.8].data - self[3.9].data
-    ch3b = self[10.8].data
+    ch1b = self["IR_120"].data - self["IR_108"].data
+    ch2b = self["IR_108"].data - self["IR_039"].data
+    ch3b = self["IR_108"].data
 
     # this area exception is not nice!
     if downscale or (self["HRV"].area.name=='ccs4' or self["HRV"].area.name=='Switzerland_stereographic_500m'):
